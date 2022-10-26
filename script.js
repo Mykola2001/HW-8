d228193e0be10426bc7cb2ab7d5e0ede039828d5
Mykola2001 <108090444+Mykola2001@users.noreply.github.com>
2022-10-26 09:25:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let number = prompt("Write here number whatever you wish");
+
+let resoult;
+
+if (number >= 14 && number <= 90) {
+    resoult = true 
+}
+else {
+    resoult = false
+}
+
+const alertBoolean = alert(resoult);
+
